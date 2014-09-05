5fc9b761608d0f589f94d7a10db4deb3a09021d9
Rick Bowen <rick@bowenux.com>
2014-09-04 21:16:28 -0400

    Initial commit

---
+++ Unit3Lesson1/Unit3Lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit3Lesson1
+//
+//  Created by Rick Bowen on 9/4/14.
+//  Copyright (c) 2014 Rick Bowen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
